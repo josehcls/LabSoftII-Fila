799c1b778c6f601a89ea77e2cd0515c874606539
jose henrique leopoldo e silva <josehcls@gmail.com>
2016-10-27 11:02:41 -0200

    Funcao Posicao

--- carrinho/restapp/models.py
+++ carrinho/restapp/models.py
@@ -2,31 +2,30 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# import Queue
-from collections import deque
+import Queue
+# from collections import deque
 
 # Create your models here.
 
 class ControleFila():
 	def __init__(self):
-		# self.fila = Queue.Queue()
-		self.fila = deque()
+		self.fila = Queue.Queue()
+		# self.fila = deque()
+		# self.fila = []
 
 	def put (self, item):
-		# self.fila.put(item)
-		self.fila.append(str(item))
+		if (self.posicao(item) == 0):
+			self.fila.put(item)
+		# self.fila.append(str(item))
+		# self.fila.append(item)
 
 	def size (self):
-		# return (self.fila.qsize())
-		return (len(self.fila))
+		return (self.fila.qsize())
+		# return (len(self.fila))
+		# return len(self.fila)
 
 	def posicao(self, id):
-		aux = self.fila
-		i = len(self.fila)
-		while(len(self.fila) != 0):
-			if (aux.pop == id):
-				return i
-			else:
-				i = i-1
+		for i in range (self.fila.qsize()):
+			if id == self.fila.queue[i]:
+				return (i+1)
 		return (0)
-		# return (self.fila.index(id))
